ded505b8f8feeb5335b4c3b0c8780e1a1ba6c060
ErykTavares <erictavares35@gmail.com>
2025-07-16 09:15:35 -0300

    feat: add Material-UI and Emotion dependencies; update App component with MUI layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-const App = () => {
-  return <></>;
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const App = () => {  
+	return (
+		<Container maxWidth='sm'>
+			<Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+				<Typography variant='h1' component='h2'>
+					teste header
+				</Typography>
+			</Box>
+		</Container>
+	);
 };
 
 export default App;
