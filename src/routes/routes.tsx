86db2d69ac153762cd3c4e09633eb1a82a6c9b84
ErykTavares <erictavares35@gmail.com>
2025-07-17 10:39:01 -0300

    feat: added History to routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 
 import Dashboard from '@/pages/private/Dashboard';
+import History from '@/pages/private/History';
 import NewVisitor from '@/pages/private/newVisitor';
 import Login from '@/pages/public/Login';
 
@@ -15,6 +16,7 @@ const AppRoutes = () => {
 				<Route element={<PrivateRoute />}>
 					<Route path='/' element={<Dashboard />} />
 					<Route path='/new-visitor' element={<NewVisitor />} />
+					<Route path='/history' element={<History />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
